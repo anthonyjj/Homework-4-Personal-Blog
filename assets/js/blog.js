f0e436a6c3d0e329a73d872bc86e6961e171ab83
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-03-17 01:02:48 -0700

    added a working dark-mode button

---
+++ assets/js/blog.js
@@ -0,0 +1,21 @@
+let darkModeEnabled = false;
+
+const darkModeButton = document.getElementById("darkModeButton");
+
+darkModeButton.addEventListener("click", () => {
+    darkModeEnabled = !darkModeEnabled;
+
+    if(darkModeEnabled) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
+
+const enableDarkMode = () => {
+    document.body.classList.add("dark-mode");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("dark-mode");
+}
