866c2913a30a854b82eaadc12d5320a80cffca8a
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-03-18 11:06:21 -0700

    added css and javascript to make submit button take you to blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -19,3 +19,36 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
     document.body.classList.remove("dark-mode");
 }
+
+// Get the input values from the first page
+const urlParams = new URLSearchParams(window.location.search);
+const blogTitle = urlParams.get('title');
+const blogContent = urlParams.get('content');
+
+// Update the blog title and content
+const titleElement = document.querySelector('.blog-title');
+const contentElement = document.querySelector('.content');
+
+if (blogTitle) {
+    titleElement.textContent = blogTitle;
+}
+
+if (blogContent) {
+    contentElement.textContent = blogContent;
+}
+
+function navigateToBlog() {
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    // Encode the user input to make it URL-safe
+    const encodedTitle = encodeURIComponent(title);
+    const encodedContent = encodeURIComponent(content);
+
+    // Construct the URL with query parameters
+    const url = `blog.html?username=${username}&title=${encodedTitle}&content=${encodedContent}`;
+
+    // Redirect the user to the blog page
+    window.location.href = url;
+}
